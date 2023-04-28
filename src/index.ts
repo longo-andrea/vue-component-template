3dd90ec824f050dbf19438796c2276defa2b4d65
longo-andrea <longoandrea.dev@gmail.com>
2023-04-28 23:43:39 +0200

    Update types generation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-export { default as ComponentA  } from './components/componentA/ComponentA.vue'
-export { default as ComponentB } from './components/componentB/ComponentB.vue'
+import ComponentA  from './components/componentA/ComponentA.vue'
+import ComponentB from './components/componentB/ComponentB.vue'
+import add from './utils/add/add'
+import multiply from './utils/multiply/multiply'
 
-export { default as add } from './utils/add/add'
-export { default as multiply } from './utils/multiply/multiply'
+export {
+    ComponentA, ComponentB, add, multiply
+}
